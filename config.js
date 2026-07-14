c678b64640edb4febb8fa46bdd95547f94d889ec
Epix69 <91039475+Epix69@users.noreply.github.com>
2026-07-14 21:05:45 +0600

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ module.exports = {
   customEmoji: true,  // true = use custom emoji IDs from emoji.js, false = use default unicode
   emojiTheme: "redwhite", // active custom emoji theme key in emoji.js
   helpBannerUrl: "https://cdn.discordapp.com/attachments/1285343206921338890/1526599851482153050/New_Project.jpg?ex=6a579c67&is=6a564ae7&hm=e2596269defac4cc8e33d9b3c3288dece8fcfb288927c300cdd5af0554d10caf&", // Optional: set a direct image URL to show an inline banner in /help
-  activityName: "YouTube Music", 
+  activityName: "JUICE GANG ON TOP", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/nszGzeNSZa",
   embedTimeout: 5,
   showProgressBar: false,  // Show progress bar in track embed
   showVisualizer: false,  // Show visualizer on music card (disabled for low-memory optimization)
@@ -25,11 +25,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
       {
-      name: "GlaceYT",
-      password: "glace",
-      host: "de-01.strixnodes.com",
-      port: 2010,
-      secure: false
+  name: "GlaceYT-2",
+  password: "glace",
+  host: "de-01.strixnodes.com",
+  port: 2028,
+  secure: false
     }
   ]
 }
